d82fa4c8ab95514e1322ccde9be9e26539dd7814
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2014-06-04 15:29:53 +0200

    Add skill query via POST

--- src/main/java/fr/xebia/xskillz/SkillRepository.java
+++ src/main/java/fr/xebia/xskillz/SkillRepository.java
@@ -1,13 +1,25 @@
 package fr.xebia.xskillz;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.base.Splitter;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
+import java.util.function.Consumer;
+
+import static com.google.common.base.Splitter.on;
+import static com.google.common.collect.FluentIterable.from;
+import static com.google.common.collect.Iterators.transform;
 
 @Path("/skill")
 public class SkillRepository {
@@ -24,6 +36,31 @@ public class SkillRepository {
     @Produces(MediaType.APPLICATION_JSON)
     public Response put(Skill skill) {
         return skills.add(skill) ? Response.status(Response.Status.CREATED).build() : Response.status(Response.Status.NOT_MODIFIED).build();
+    }
 
+    @POST
+    @Path("/search")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Skill> search(String query) {
+
+        final List<Predicate<Skill>> predicates = new ArrayList<>();
+
+        Splitter.on(' ').trimResults().split(query).forEach(new Consumer<String>() {
+            @Override
+            public void accept(final String searchItem) {
+                predicates.add(new Predicate<Skill>() {
+                    @Override
+                    public boolean apply(Skill o) {
+                        return o.getName().toLowerCase().contains(searchItem.toLowerCase());
+                    }
+                });
+            }
+        });
+
+        Predicate<Skill> skillPredicate = Predicates.or(predicates);
+
+        return from(skills)
+                .filter(skillPredicate)
+                .toList()   ;
     }
 }
